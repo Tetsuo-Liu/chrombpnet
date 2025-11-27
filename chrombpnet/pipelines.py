fix(hyperparams): skip bias scaling in celltype_aggregate mode
Skip adjust_bias_model_logcounts and bias_model_scaled.h5 creation
when using celltype_aggregate data generator, as dynamic scaling
handles bias correction during training. Use original bias model
path directly in chrombpnet_model_params.tsv.

chrombpnet/pipelines.py
@@ -66,15 +66,22 @@ def chrombpnet_train_pipeline(args):
 	print("Bias model pearsonr performance in peaks is: {}".format(str(np.round(bias_metrics["counts_metrics"]["peaks"]["pearsonr"],2))))
 	assert(bias_metrics["counts_metrics"]["peaks"]["pearsonr"] > -0.5) # bias model has negative correlation in peaks - AT rich bias model. Increase bias threshold and retrain bias model. Or use a different bias model with higher bias threshold. 
 	
+	# Check if celltype_aggregate mode is enabled
+	use_celltype_aggregate = hasattr(args, 'data_generator_type') and args.data_generator_type == 'celltype_aggregate'
+	
 	# separating models from logs
-	os.rename(os.path.join(args.output_dir,"auxiliary/{}bias_model_scaled.h5".format(fpx)),os.path.join(args.output_dir,"models/{}bias_model_scaled.h5".format(fpx)))
+	# In celltype_aggregate mode, bias_model_scaled.h5 is not created (original bias model is used)
+	if not use_celltype_aggregate:
+		os.rename(os.path.join(args.output_dir,"auxiliary/{}bias_model_scaled.h5".format(fpx)),os.path.join(args.output_dir,"models/{}bias_model_scaled.h5".format(fpx)))
 	os.rename(os.path.join(args.output_dir,"auxiliary/{}chrombpnet_model_params.tsv".format(fpx)),os.path.join(args.output_dir,"logs/{}chrombpnet_model_params.tsv".format(fpx)))
 	os.rename(os.path.join(args.output_dir,"auxiliary/{}chrombpnet_data_params.tsv".format(fpx)),os.path.join(args.output_dir,"logs/{}chrombpnet_data_params.tsv".format(fpx)))
 
-	params = open(os.path.join(args.output_dir,"logs/{}chrombpnet_model_params.tsv".format(fpx))).read()
-	params = params.replace(os.path.join(args.output_dir,"auxiliary/{}bias_model_scaled.h5".format(fpx)),os.path.join(args.output_dir,"models/{}bias_model_scaled.h5".format(fpx)))
-	with open(os.path.join(args.output_dir,"logs/{}chrombpnet_model_params.tsv".format(fpx)),"w") as f:
-		f.write(params)
+	# Update bias_model_path in params file if bias_model_scaled.h5 was created
+	if not use_celltype_aggregate:
+		params = open(os.path.join(args.output_dir,"logs/{}chrombpnet_model_params.tsv".format(fpx))).read()
+		params = params.replace(os.path.join(args.output_dir,"auxiliary/{}bias_model_scaled.h5".format(fpx)),os.path.join(args.output_dir,"models/{}bias_model_scaled.h5".format(fpx)))
+		with open(os.path.join(args.output_dir,"logs/{}chrombpnet_model_params.tsv".format(fpx)),"w") as f:
+			f.write(params)
 		
 	# get model architecture path and train chromBPNet model
 	import chrombpnet.training.models.chrombpnet_with_bias_model as chrombpnet_with_bias_model